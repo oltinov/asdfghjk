bd053b6b5aaafb0da865715c7ff50e4d8c9cec29
Muhammadali Oltinov <muhammadalioltinov@gmail.com>
2024-11-01 12:11:42 +0500

    first

---
+++ src/main/java/org/example/asdfghjkl/AsdfghjklApplication.java
@@ -0,0 +1,13 @@
+package org.example.asdfghjkl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdfghjklApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsdfghjklApplication.class, args);
+    }
+
+}
